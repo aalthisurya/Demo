a54be796051bb0f7cc24c1c82b9948361bfe0ce1
aalthisurya <32411284+aalthisurya@users.noreply.github.com>
2019-01-30 16:46:00 +0530

    Add files via upload

---
+++ SlopeCheck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+
+
+public class SlopeCheck : MonoBehaviour {
+
+	public GameObject Temp;
+
+	public Vector3 a;
+
+	public Quaternion b;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		b = this.GetComponent<VRTK_StraightPointerRenderer> ().Quat.rotation;
+		a = this.GetComponent<VRTK_StraightPointerRenderer> ().TempPost;
+		Temp.transform.position = new Vector3 (a.x,a.y,a.z);
+		Temp.transform.rotation = new Quaternion (b.x,b.y,b.z,b.w);
+	}
+
+
+
+}
